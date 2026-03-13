5ea68be133bb21bb29c04ff864b96f21657020ab
Alex <161225970+takeomeyr@users.noreply.github.com>
2026-03-13 20:23:00 +0300

    Refactor Nix configuration for improved structure

--- configuration.nix
+++ configuration.nix
@@ -1,31 +1,35 @@
-{ inputs, config, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+  # Imports:
   imports = [
     ./hardware-configuration.nix
   ];
 
+  system.stateVersion = "25.11";
+
+  # Boot:
   boot.loader.grub = {
     enable = true;
     device = "/dev/sda";
     useOSProber = true;
   };
 
+  # Networking:
   networking = {
-    hostName = "alex";
+    hostName = "nix-pc";
     networkmanager.enable = true;
-
     firewall = {
-      enable = false;
-      allowedTCPPorts = [ ];
+      enable = true;
+      allowedTCPPorts = [ 22 ];
     };
   };
 
+  # Time & Locale:
   time.timeZone = "Europe/Moscow";
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
-
     extraLocaleSettings = {
       LC_ADDRESS = "ru_RU.UTF-8";
       LC_IDENTIFICATION = "ru_RU.UTF-8";
@@ -39,38 +43,11 @@
     };
   };
 
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us";
-    videoDrivers = [ "amdgpu" ];
-    libinput.enable = true;
-
-    displayManager.gdm = {
-      enable = true;
-      wayland = true;
-    };
-
-    desktopManager.gnome.enable = true;
-  };
-
-  hardware.pulseaudio.enable = false;
-
-  security.rtkit.enable = true;
-
-  services.pipewire = {
-    enable = true;
-
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
-
-    pulse.enable = true;
-  };
-
+  # Users:
   users.users.alex = {
     isNormalUser = true;
     description = "Alex";
+
     extraGroups = [
       "wheel"
       "networkmanager"
@@ -78,47 +55,129 @@
     ];
   };
 
+  # Hardware:
+  hardware = {
+    pulseaudio.enable = false;
+    graphics.enable = true;
+  };
+  security.rtkit.enable = true;
+
+  # Virtualisation:
   virtualisation.docker.enable = true;
 
+  # Services:
   services = {
-    flatpak.enable = true;
-    openssh.enable = true;
+    # System maintenance:
     fstrim.enable = true;
+    fwupd.enable = true;
+
+    # Printing and filesystem:
     printing.enable = true;
     gvfs.enable = true;
     tumbler.enable = true;
+
+    # Flatpak:
+    flatpak.enable = true;
+
+    # Logging:
+    journald.extraConfig = ''
+      SystemMaxUse=500M
+      RuntimeMaxUse=200M
+    '';
+
+    # SSH:
+    openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+
+    # Audio:
+    pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+    };
+
+    # X11 / GNOME:
+    xserver = {
+      enable = true;
+      xkb.layout = "us";
+      videoDrivers = [ "amdgpu" ];
+      libinput.enable = true;
+      displayManager.gdm = {
+        enable = true;
+        wayland = true;
+      };
+      desktopManager.gnome.enable = true;
+    };
   };
 
-  environment.localBinInPath = true;
+  # Desktop portals:
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
 
+  # Programs:
   programs = {
     firefox.enable = true;
     thunar.enable = true;
     mtr.enable = true;
-
+    bash.enableCompletion = true;
+    dconf.enable = true;
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    wget
-    curl
-    fastfetch
-  ];
+  # Packages:
+  environment = {
+    localBinInPath = true;
+    systemPackages = with pkgs; [
+      vim
+      git
+      wget
+      curl
+      fastfetch
+      kitty
+      btop
+      ripgrep
+      fd
+      tree
+      unzip
+      zip
+    ];
+  };
 
+  # Performance:
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;
+  };
+
+  # Nix:
   nix = {
     useChroot = true;
-
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = [
+        "nix-command"
+        # "flakes"
+      ];
       auto-optimise-store = true;
-      trusted-users = [ "root" "alex" ];
+      trusted-users = [
+        "root"
+        "alex"
+      ];
+      keep-outputs = true;
+      keep-derivations = true;
     };
-
     gc = {
       automatic = true;
       dates = "weekly";
@@ -126,12 +185,22 @@
     };
   };
 
+  # Sudo:
+  security.sudo = {
+    enable = true;
+
+    extraConfig = ''
+      Defaults timestamp_timeout=30
+      Defaults pwfeedback
+    '';
+  };
+
+  # Auto Upgrade:
   system.autoUpgrade = {
     enable = true;
     allowReboot = false;
   };
 
+  # Nixpkgs:
   nixpkgs.config.allowUnfree = true;
-
-  system.stateVersion = "25.11";
 }
