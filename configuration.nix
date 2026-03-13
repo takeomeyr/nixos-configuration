df30f66ff7b6931d47fd8dad7ff7d6ebbb762d49
Alex <161225970+takeomeyr@users.noreply.github.com>
2026-03-13 22:47:27 +0300

    Update firewall configuration

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,8 @@
     networkmanager.enable = true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 ];
+      allowedTCPPorts = [ 22 80 443 ];   # common allowed TCP ports
+      allowedUDPPorts = [ ];             # allowed UDP ports
     };
   };
 
